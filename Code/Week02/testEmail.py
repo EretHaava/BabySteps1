252fe7730ac879463c0b68999d6981957e4d2bf0
EretHaava <eret.haava@gmail.com>
2022-01-29 19:31:47 +0000

    Week02

---
+++ Code/Week02/testEmail.py
@@ -0,0 +1,15 @@
+# send an email via Gmail
+# author Eret Haava
+
+print ('in testEmail')
+import smtplib
+
+username = 'eret.haava@gmail.com'
+password = 'EretHaava261280'
+toEmail = 'eret.haava@gmail.com'
+message = 'Hi There!'
+
+server = smtplib.SMTP('smtp.gmail.com',587)
+server.starttls()
+server.login(username,password)
+server.sendmail(username,toEmail,message)
